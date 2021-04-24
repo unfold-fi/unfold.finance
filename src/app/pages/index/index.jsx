0a50cbdd61dbac7879f9fda1765aeb9c8c41f03b
Andrew U <tmyx0m0p@gmail.com>
2021-04-24 17:40:24 +0300

    add links

--- src/app/pages/index/index.jsx
+++ src/app/pages/index/index.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import PrimaryButton from '../../components/primaryButton';
 
@@ -8,8 +9,12 @@ const IndexPage = () => (
       NFT assets collateralized lending and rent protocol on Ethereum
     </Heading>
     <ButtonContainer>
-      <PrimaryButton sx={{ type: 'outline' }}>Learn more</PrimaryButton>
-      <PrimaryButton sx={{ type: 'solid' }}>Enter app</PrimaryButton>
+      <Link to="">
+        <PrimaryButton sx={{ type: 'outline' }}>Learn more</PrimaryButton>
+      </Link>
+      <Link to="/app">
+        <PrimaryButton sx={{ type: 'solid' }}>Enter app</PrimaryButton>
+      </Link>
     </ButtonContainer>
   </Container>
 );
