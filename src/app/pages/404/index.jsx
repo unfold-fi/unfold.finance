e95407b85a52a2185e37598ab4c43c0058d80682
Andrew U <tmyx0m0p@gmail.com>
2021-04-24 17:40:17 +0300

    404 page

--- src/app/pages/404/index.jsx
+++ src/app/pages/404/index.jsx
@@ -1,20 +1,41 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import PrimaryButton from '../../components/primaryButton';
 
 const NotFoundPage = () => (
   <Container>
-    <div className="container pt-16 md:pt-32 m-auto flex flex-wrap flex-col md:flex-row items-center">
-      Not found
-    </div>
+    <Heading>404: Page not found</Heading>
+    <ButtonContainer>
+      <Link to="/">
+        <PrimaryButton sx={{ type: 'outline' }}>Back to main</PrimaryButton>
+      </Link>
+    </ButtonContainer>
   </Container>
 );
 const Container = styled.div`
-  display: grid;
-  grid-auto-flow: row;
-  gap: 5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+  justify-content: center;
   @media (max-width: 48rem) {
-    gap: 1.875rem;
+    gap: 0.625rem;
   }
+`;
+const Heading = styled.h1`
+  font-size: 3.5rem;
+  line-height: 4rem;
+  color: #4b58df;
+  max-width: 50rem;
+  font-weight: 400;
+  @media (max-width: 48rem) {
+    font-size: 2.25rem;
+    line-height: 2.5rem;
+  }
+`;
+const ButtonContainer = styled.div`
+  display: flex;
+  gap: 1.5rem;
 `;
 
 export default NotFoundPage;
